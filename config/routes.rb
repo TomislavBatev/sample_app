0a563a569ed68fdabbb4a54c51c12ae09cc6dec9
TomislavBatev <toma@tariffic.com>
2013-06-25 08:19:26 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users 
 
   root to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
@@ -7,6 +7,8 @@ SampleApp::Application.routes.draw do
   match '/contact', to: 'static_pages#contact'
 
   match '/signup',  to: 'users#new'
+
+
 
 
   # The priority is based upon order of creation:
